3d58af922e092ab4d3011a2db90cd48741774a0e
Neha Sandhu <749651@student.tor.loc>
2019-06-19 16:33:14 -0400

    first commit

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-console.log('hello class');
+function createCircle(radius){
+return{
+    radius,
+    draw:function(){
+        console.log('draw');
+    }
+};
+}
+const circle=createCircle(1);
+
+function Circle(radius){
+    console.log('this',this);
+    this.radius=radius;
+    this.draw=function(){
+        console.log('draw');
+    }
+}
+const another =new Circle(1);
+
+const Circle1= new Function('radius',`
+this.radius=radius;
+    this.draw=function(){
+        console.log('draw');
+    }
+    `);
+   
+    Circle.call
+    const orange = new Circle(1);
